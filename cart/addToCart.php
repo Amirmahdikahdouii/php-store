6e49dff41d414424ad06564ddc3b48d3513fecae
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2022-08-14 17:01:44 +0430

    Remove addtocart.php and add addToCart.php with new changes

---
+++ cart/addToCart.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+// Get Product Id From Query String
+if (
+isset($_GET["product_id"])
+) {
+    $productId = intval($_GET["product_id"]);
+} else {
+    $productId = 0;
+}
+try {
+    if ($productId === 0) {
+        throw new Exception("Product Id Is not Valid");
+    }
+} catch (Exception $ex) {
+    die($ex->getMessage());
+}
+// Check If User Is Authenticated, Add Product To Cart
+if (
+    isset($_SESSION["user_login"]) &&
+    isset($_SESSION["user_id"])
+) {
+    require_once "../settings/dbconfig.php";
+    $userID = intval($_SESSION["user_id"]);
+    // Get User Cart From Database
+    try {
+        $sql = "SELECT * FROM cart WHERE user_id=$userID";
+        $result = mysqli_query($db_connection, $sql);
+        $result = mysqli_fetch_assoc($result);
+        if (!$result) {
+            // Raise New Exception when User Doesnt have Cart
+            throw new Exception("User Doesnt have any Cart in Database");
+        }
+    } catch (Exception $ex) {
+        // Add New Cart For User in cart
+        $sql = "INSERT INTO `cart` (`id`, `user_id`) VALUES (NULL, '$userID');";
+        $result = mysqli_query($db_connection, $sql);
+        $result = mysqli_fetch_assoc($result);
+    }
+    // Get The User Cart Id
+    $userCartId = $result["id"];
+    try {
+        // Trying to get Field of Specified User and Product, Because If USer Have Already Product In His Cart, Add Count of that
+        $sql = "SELECT * FROM `cart_products` WHERE product_id='$productId' AND cart_id='$userCartId';";
+        $result = mysqli_query($db_connection, $sql);
+        $result = mysqli_fetch_assoc($result);
+        if (!$result) {
+            // Raise Exception to Make new Field For Product In user cart
+            throw new Exception("Product Is Not in the User Cart");
+        }
+        $productCount = intval($result["count"]) + 1;
+        // Maximum Count of buying product handle
+        if ($productCount > 10) {
+            die("Maximum of Product Count For Buy!");
+        }
+        // Update Product In User Cart
+        $productCartId = $result["id"];
+        // TODO: Minus Count Of Product
+        $sql = "UPDATE `cart_products` SET count='$productCount' WHERE id='$productCartId'";
+        $result = mysqli_query($db_connection, $sql);
+    } catch (Exception $ex) {
+        // Raise Exception If User Have Not Product In His cart_product And Add New Field With Count 1
+        // TODO: Minus Count Of Product
+        $sql = "INSERT INTO `cart_products` (`id`, `product_id`, `cart_id`, `count`) VALUES (NULL, '$productId', '$userCartId', '1')";
+        $result = mysqli_query($db_connection, $sql);
+    }
+}
