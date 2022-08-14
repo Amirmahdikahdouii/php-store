f623ad0ee65529f4153236bf4d886fa26ed9ec85
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2022-08-14 23:07:22 +0430

    Alert Component added and work with ajax and JavaScript to show new alert box to user with custom messages and handler, create main scripts become a component to include in every module

--- components/header-footer-style.php
+++ components/header-footer-style.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-<link rel="stylesheet" href="../static/css/header.css">
-<link rel="stylesheet" href="../static/css/footer.css"/>
+<link rel="stylesheet" href="../static/css/components/header.css">
+<link rel="stylesheet" href="../static/css/components/alertMessage.css"/>
+<link rel="stylesheet" href="../static/css/components/footer.css"/>
