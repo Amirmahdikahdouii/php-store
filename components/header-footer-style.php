ac1b441a9a32b9593833a55b2aadde8958a4c6e5
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2022-08-17 22:35:29 +0430

    Make Footer As a component and make some changes in files and styles

--- components/header-footer-style.php
+++ components/header-footer-style.php
@@ -1,3 +1,6 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="../static/css/components/header.css">
 <link rel="stylesheet" href="../static/css/components/alertMessage.css"/>
